0623b5f2a320af93711cd0a9ca92d8fb7a2c175c
Dean Attali <dean@attalitech.com>
2021-02-01 01:00:35 -0500

    slow down particles.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -56,7 +56,7 @@ $(function() {
         },
         "move": {
           "enable": true,
-          "speed": 6,
+          "speed": 4,
           "direction": "none",
           "random": false,
           "straight": false,
